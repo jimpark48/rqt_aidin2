091daec93e3bc46a5700fc65b9ce253fe74892fc
ymove010 <yhlee010@gmail.com>
2019-02-14 20:18:07 +0900

    --screen deleted

--- rqt_aidin2/src/rqt_aidin2.cpp
+++ rqt_aidin2/src/rqt_aidin2.cpp
@@ -89,7 +89,7 @@ void aidinPlugin2::on_pushButton_clicked1()
     if(value != 10000) {
       const char *message2 = "gnome-terminal -e 'roslaunch "; //new terminal
       //const char *message3 = " --screen'";
-      const char *message3 = " '";
+      const char *message3 = "'";
       const char *message4 = sumstring(message2, message1);
       message4 = sumstring(message4, message3);
       ROS_INFO("%s", message4);
